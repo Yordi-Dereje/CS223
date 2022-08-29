48b1b4c3a6b615697507450fc34dad4aa0ce72e1
Yordanos Dereje <yordadereje@gmail.com>
2022-08-29 23:17:45 +0300

    Sql file added

---
+++ Db.sql
@@ -0,0 +1,11 @@
+use csharpdb;
+create table ProjectTable(
+Number int,
+Date varchar(30),
+Inventory_Number int,
+Object_Name varchar(30),
+Count int,
+Price double,
+Availability varchar(6),
+OrderComplete varchar(6)
+);
