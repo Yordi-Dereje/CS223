549d7df31a50e35c4c38627cf67c18ad1452b33f
Yordanos Dereje <yordadereje@gmail.com>
2022-09-03 22:45:39 +0300

    Update DB.sql

--- Db.sql
+++ Db.sql
@@ -1,5 +1,5 @@
-use csharpdb;
-create table ProjectTable(
+USE csharpdb;
+CREATE TABLE ProjectTable(
 Number int,
 Date varchar(30),
 Inventory_Number int,
@@ -9,3 +9,25 @@ Price double,
 Availability varchar(6),
 OrderComplete varchar(6)
 );
+
+DELIMITER //
+CREATE PROCEDURE InsertProc(n int, d varchar(50), i int, o varchar(30), c int, p double, av bit, oc bit)
+BEGIN
+    INSERT INTO ProjectTable VALUES(n, d, i, o, c, p, av, oc);
+END //
+DELIMITER;
+
+
+DELIMITER //
+CREATE PROCEDURE updateProc(d varchar(50), i int, o varchar(30), c int, p double, av bit, oc bit, n int)
+BEGIN
+    UPDATE ProjectTable SET Date=d, Inventory_Number=i, Object_Name=o, Count=c, Price=p, Availability=av, OrderComplete=oc WHERE Number=n;
+END //
+DELIMITER;
+
+
+DELIMITER //
+CREATE PROCEDURE deleteProc(n int)
+BEGIN
+    DELETE FROM ProjectTable WHERE Number=n;
+END //
